57a931747a201da0a09c7d8b250617553934f6d4
Zayaan <xayaani@gmail.com>
2019-09-15 15:03:03 +0500

    Update ventures.js

--- src/templates/ventures.js
+++ src/templates/ventures.js
@@ -4,7 +4,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const VenturesTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
@@ -25,18 +25,18 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   )
 }
 
-AboutPageTemplate.propTypes = {
+VenturesTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
 
-const AboutPage = ({ data }) => {
+const Ventures = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
     <Layout>
-      <AboutPageTemplate
+      <VenturesTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
@@ -45,14 +45,14 @@ const AboutPage = ({ data }) => {
   )
 }
 
-AboutPage.propTypes = {
+Ventures.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-export default AboutPage
+export default Ventures
 
-export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
+export const venturesQuery = graphql`
+  query Ventures($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
